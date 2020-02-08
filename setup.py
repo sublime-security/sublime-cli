f75f70db8daee2163b227abeaa3911c5918e114a
Josh Kamdjou <jkamdjou@gmail.com>
2020-02-07 21:33:56 -0500

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="sublime",
-    version="0.0.6",
+    version="0.0.7",
     description="Abstraction to interact with Sublime API.",
     url="https://sublimesecurity.com/",
     author="Sublime Security",
