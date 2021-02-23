82d052cdd745fbaa31d8be46d7f3fd0e08b83601
blee <brianlee2795@gmail.com>
2021-02-23 12:25:19 -0500

    Update Analyze subcommand to return proper results for filepaths

--- src/sublime/cli/subcommand.py
+++ src/sublime/cli/subcommand.py
@@ -109,34 +109,48 @@ def analyze(
         LOGGER.error("YML file or raw MQL required")
         context.exit(-1)
 
+    # sort rules and queries in advance so we don't have to later
+    # analyze endpoint should conserve the order in which they're submitted
+    rules = sorted(rules, key=lambda i: i["name"].lower() if i.get("name") else "")
+    queries = sorted(queries, key=lambda i: i["name"].lower() if i.get("name") else "")
+
     # aggregate all files we need to check 
-    input_files = []
+    file_paths = []
     if os.path.isfile(input_path):
-        input_files.append(input_path)
+        file_paths.append(input_path)
+    else:
+        for file_path in Path(input_path).rglob("*.eml"):
+            file_paths.append(str(file_path))
+        for file_path in Path(input_path).rglob("*.msg"):
+            file_paths.append(str(file_path))
+        for file_path in Path(input_path).rglob("*.mdm"):
+            file_paths.append(str(file_path))
 
-    # else:
-    #     for rules_file in Path(files_path).rglob("*.yml"):
-
-    # iterate through all input files and aggregate results
-    with Halo(text='Analyzing...', spinner='dots'):
-        for input_file in input_files:
-            if input_file.endswith(".mdm"):
-                message_data_model = load_message_data_model(input_file)
-                response = api_client.analyze_message(
+    # analyze each file and aggregate all responses
+    responses = {}
+    for file_path in file_paths:
+        file_dir, _, file_name = file_path.rpartition('/')
+        with Halo(
+            text=f'Analyzing {file_name} in {file_dir}...',
+            spinner='dots'
+        ):
+            if file_path.endswith(".mdm"):
+                message_data_model = load_message_data_model(file_path)
+                responses[file_path] = api_client.analyze_message(
                         message_data_model,
                         rules,
                         queries)
-            elif input_file.endswith(".msg"):
-                raw_message = load_msg(input_file)
-                response = api_client.analyze_raw_message(
+            elif file_path.endswith(".msg"):
+                raw_message = load_msg(file_path)
+                responses[file_path] = api_client.analyze_raw_message(
                         raw_message, 
                         rules,
                         queries,
                         mailbox_email_address,
                         message_type)
-            elif input_file.endswith(".eml"):
-                raw_message = load_eml(input_file)
-                response = api_client.analyze_raw_message(
+            elif file_path.endswith(".eml"):
+                raw_message = load_eml(file_path)
+                responses[file_path] = api_client.analyze_raw_message(
                         raw_message, 
                         rules,
                         queries,
@@ -146,14 +160,23 @@ def analyze(
                 LOGGER.error("Input file must have .eml, .msg or .mdm extension")
                 context.exit(-1)
 
-    results = {}
-    for result_key in ["rule_results", "query_results"]:
-        results_list = response.get(result_key) if response.get(result_key) else []
-        results[result_key] = sorted(
-                results_list,
-                key=lambda i: i["name"].lower() if i.get("name") else "")
+    # sort rules
+    # results = {}
+    # for input_file, response in responses.items():
+    #     for result_key in ["rule_results", "query_results"]:
+    #         results_list = response.get(result_key) if response.get(result_key) else []
+    #         results[result_key] = sorted(
+    #                 results_list,
+    #                 key=lambda i: i["name"].lower() if i.get("name") else "")
     
-    return results
+    # for key in responses:
+    #     for rr in responses[key]["rule_results"]:
+    #         if rr:
+    #             rr["source"] = ""
+    # print(json.dumps(responses, indent=4))
+    
+    # print(json.dumps(results, indent=4))
+    return responses
 
 
 @me_command
