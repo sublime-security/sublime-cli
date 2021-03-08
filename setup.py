9c1e9d1b375a32fbdd2028c7837b35e7272cc0ad
Josh Kamdjou <jkamdjou@gmail.com>
2021-03-08 16:12:01 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="sublime-cli",
-    version="0.0.25",
+    version="0.0.26",
     description="Abstraction to interact with the Sublime API.",
     url="https://sublimesecurity.com/",
     author="Sublime Security",
