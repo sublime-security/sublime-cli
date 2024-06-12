bdc943105da4cee658478709303b54af5634a8ea
andrew-kline <65789862+andrew-kline@users.noreply.github.com>
2024-06-12 08:21:40 -0700

    Add run_all_insights to analyze message function (#36)
    * feat: add run_all_insights to analyze message function
    
    * chore: version bump
    
    ---------
    
    Co-authored-by: Andrew Kline <andrew.kline@getcruise.com>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="sublime-cli",
-    version="0.0.30",
+    version="0.0.31",
     description="Abstraction to interact with the Sublime API.",
     url="https://sublimesecurity.com/",
     author="Sublime Security",
